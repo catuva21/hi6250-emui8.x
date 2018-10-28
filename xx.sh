0afa17a1f62a139bde6c1051b73d03c962932959
tucno21 <carlitostucno@gmail.com>
2018-10-28 04:56:45 -0500

    clear xx

--- xx.sh
+++ xx.sh
@@ -90,8 +90,11 @@ case $ch in
      echo -e "***********************************************$nocol"
      make ARCH=arm64 distclean
      rm -rf ../out
-cp -r ~/Escritorio/Files/Source_files/libperl.a drivers/huawei_platform/lcd/tools/localperl/lib/5.14.2/x86_64-linux-thread-multi/CORE/libperl.a
-     make_kernel ;;
+cp -r ~/Escritorio/Akernels/Files/libperl.a drivers/huawei_platform/lcd/tools/localperl/lib/5.14.2/x86_64-linux-thread-multi/CORE/libperl.a
+     cp -r ~/Escritorio/Akernels/Files/zconf.hash.c scripts/kconfig/zconf.hash.c
+     cp -r ~/Escritorio/Akernels/Files/zconf.lex.c scripts/kconfig/zconf.lex.c
+     cp -r ~/Escritorio/Akernels/Files/zconf.tab.c scripts/kconfig/zconf.tab.c
+	 make_kernel ;;
 esac
 
      echo
